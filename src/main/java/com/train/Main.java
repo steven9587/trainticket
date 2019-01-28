94eadc1e302388e86b5514e28bbda5c46e73d4d3
steven9587 <steven9587@gmail.com>
2019-01-28 16:46:03 +0800

    20190128javaFinal

--- src/main/java/com/train/Main.java
+++ src/main/java/com/train/Main.java
@@ -3,13 +3,23 @@ package com.train;
 import java.util.Scanner;
 
 public class Main {
+
+    private static int totaltickets;
+    private static int roundtrip;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets:");
-        int totaltickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets:");
-        int roundtrip = scanner.nextInt();
-        Ticket ticket = new Ticket(totaltickets,roundtrip);
-        ticket.print();
+        while (totaltickets != -1) {
+            System.out.print("Please enter number of tickets:");
+            totaltickets = scanner.nextInt();
+            if (totaltickets == -1)
+                break;
+            System.out.print("How many round-trip tickets:");
+            roundtrip = scanner.nextInt();
+            Ticket ticket = new Ticket(totaltickets, roundtrip);
+            ticket.print();
+            System.out.println();
+
+        }
     }
 }
