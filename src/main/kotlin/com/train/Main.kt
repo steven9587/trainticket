4ac1f86c3cff68c248d565ddbce1844e72091c05
steven9587 <steven9587@gmail.com>
2019-01-28 16:50:31 +0800

    20190128kotlinFinal

--- src/main/kotlin/com/train/Main.kt
+++ src/main/kotlin/com/train/Main.kt
@@ -3,20 +3,26 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets:");
-    var totaltickets = scanner.nextInt();
-    System.out.print("How many round-trip tickets:");
-    var roundtrip = scanner.nextInt();
-    val ticket = Coupon(totaltickets, roundtrip)
-    ticket.print()
+    var totaltickets = 0
+    while (totaltickets != -1) {
+        //val scanner = Scanner(System.`in`)
+        print("Please enter number of tickets:")
+        totaltickets = readLine()!!.toInt()
+        if (totaltickets == -1)
+            break;
+        System.out.print("How many round-trip tickets:")
+        var roundtrip = readLine()!!.toInt()
+        val ticket = Coupon(totaltickets, roundtrip)
+        ticket.print()
+        println()
+    }
 }
 
 class Coupon(var totaltickets: Int, var roundtrip: Int) {
     fun print() {
-        val price : Int = ((totaltickets-roundtrip)*1000+roundtrip*2000*0.9).toInt()
-        println("Total tickets: "+totaltickets);
-        println("Round-trip: "+roundtrip);
-        println("Total: "+price);
+        val price: Int = ((totaltickets - roundtrip) * 1000 + roundtrip * 2000 * 0.9).toInt()
+        println("Total tickets: " + totaltickets);
+        println("Round-trip: " + roundtrip);
+        println("Total: " + price);
     }
 }
